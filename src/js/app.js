5ea8e9e7a64b4d57ec5897daca5310aa0b42653f
PaataSet <paataseturidze@gmail.com>
2024-12-22 22:51:10 +0400

    add html
    add html code from JavaScript

--- src/js/app.js
+++ src/js/app.js
@@ -9,20 +9,59 @@ console.log("HomeWork_22");
     სტრუქტურა, რომ ქარდები სამ-სამ ბლოკებად იყოს განაწილებული.
 */
 
+const body = document.querySelector("body");
+const section = document.createElement("section");
+
+const newSection = document.createElement("section");
+body.append(newSection);
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((Response) => {
     console.log(Response);
     return Response.json();
   })
   .then((data) => {
-    console.log(data.name);
+    console.log(data);
+
+    const dataToUse = data;
+    console.log(dataToUse);
+    const logData = (el, index, arr) => {
+      console.log(el, index, arr);
+    };
+
+    const formatedArray = dataToUse.map((el, index, arr) => {
+      return {
+        name: el.name,
+        email: el.email,
+        address: el.address,
+      };
+    });
+
+    function renderHTML(data) {
+      const formatedData = data.map(
+        (el) =>
+          `<div class="card">
+            <h2>${el.name}</h2>
+            <p>${el.email}</p>
+            <div class="price-line">
+                  <p>${el.address}</p>
+             
+             </div> 
+            
+        
+        </div>`
+      );
+      return formatedData.join(" ");
+    }
+
+    newSection.innerHTML = renderHTML(data);
   })
   .catch((error) => {
     console.log(error);
   });
 
 /*
-const dataToUse = data.user;
+const dataToUse = data;
 console.log(dataToUse);
 const logData = (el, index, arr) => {
   console.log(el, index, arr);
