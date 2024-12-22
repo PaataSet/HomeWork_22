90aade120439bcaf89c535513364c908d6f30fea
PaataSet <paataseturidze@gmail.com>
2024-12-22 15:07:54 +0400

    add some code

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,60 @@
 console.log("HomeWork_22");
 
 /*
-
 დავალება
 
     სერვერიდან წამოიღეთ მონაცემები ამ endpoint-ის გამოყენებით https://jsonplaceholder.typicode.com/users
     ჯავასკრიპტის გამოყენებით გამოიტანეთ  შემდეგი ინფორმაცია: name, email და address;
     დაამატეთ სტილებიც რომ ვიზუალურად კარგად გამოიყურებოდეს, სასურველი იქნება გამოიყენოთ grid-template-columns: repeate(3,1fr) 
     სტრუქტურა, რომ ქარდები სამ-სამ ბლოკებად იყოს განაწილებული.
-
 */
 
 fetch("https://jsonplaceholder.typicode.com/users")
-  .then((Response) => Response.json())
+  .then((Response) => {
+    console.log(Response);
+    return Response.json();
+  })
   .then((data) => {
-    console.log(data);
+    console.log(data.name);
+  })
+  .catch((error) => {
+    console.log(error);
   });
+
+/*
+const dataToUse = data.user;
+console.log(dataToUse);
+const logData = (el, index, arr) => {
+  console.log(el, index, arr);
+};
+
+const formatedArray = dataToUse.map((el, index, arr) => {
+  return {
+    name: el.name,
+    email: el.email,
+    address: el.address,
+  };
+});
+
+function renderHTML(data) {
+  const formatedData = data.map(
+    (el) =>
+      `<div class="card">
+        <h2>${el.name}</h2>
+        <p>${el.email}</p>
+        <div class="price-line">
+              <p>${el.address}</p>
+         <div class="buttons">
+              <button class="button1">Show more</button>
+             <button class="button2">Delete</button>
+          </div>
+         </div> 
+        
+    
+    </div>`
+  );
+  return formatedData.join(" ");
+}
+
+section.innerHTML = renderHTML(data);
+*/
